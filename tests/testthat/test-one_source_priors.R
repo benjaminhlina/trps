7a1d0bdde03ba198095cee71e2af9273fd37a782
Your Correct Name Here <Your Correct Email Here>
2025-02-05 17:46:30 -0500

    Updated tests approrpaitely

--- tests/testthat/test-one_source_priors.R
+++ tests/testthat/test-one_source_priors.R
@@ -1,16 +1,15 @@
 
 
-# Example tests for the create_comparisons function
+# tests for the one_source_priors
 test_that("test one-source priors", {
 
   pri <- one_source_priors()
 
-  expected <- c("normal(9, 1)",
+  expected <- c("normal(n1, n1_sigma)",
                 "normal(3.4, 0.5)",
                 "uniform(2, 10)",
                 "uniform(0, 10)"
-
-  )
+                )
 
   results <- pri$prior
 
