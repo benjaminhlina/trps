ebf7f5fc547dea794ace4bf623de30ec1f189b2e
Your Correct Name Here <Your Correct Email Here>
2025-02-05 09:58:08 -0500

    Added in error message and argument for n1

--- R/one_source_priors.R
+++ R/one_source_priors.R
@@ -5,16 +5,35 @@
 #' @import brms
 #' @export
 
-one_source_priors <- function() {
-  c(
+one_source_priors <- function(n1 = NULL) {
+
+  # set n1 to 9
+  if (is.null(n1)) {
+    n1 <- 9
+  }
+  # create error message for n1 priros
+  if  (!is.numeric(n1)) {
+    cli::cli_abort(c(
+      "`n1` argument must be a numerical value",
+      "i" = "Please provide a numerical value to as an informed or uniformed
+      prior for n1"
+    ))
+  }
+
+
+
+  #  set priors
+  priors <- c(
     # Baseline δ15N (n1)
-    prior(normal(9.83, 1), class = "b", coef = "Intercept", nlpar = "n1"),
+    brms::prior(normal(n1, 1), class = "b", coef = "Intercept", nlpar = "n1"),
     # Trophic enrichment factor (ΔN)
-    prior(normal(3.4, 0.5), class = "b", coef = "Intercept", nlpar = "dn"),
+    brms::prior(normal(3.4, 0.5), class = "b", coef = "Intercept", nlpar = "dn"),
     # Trophic Position (TP)
-    prior(uniform(2, 10), class = "b", nlpar = "tp", lb = 2, ub = 10),
+    brms::prior(uniform(2, 10), class = "b", nlpar = "tp",
+          lb = 2, ub = 10),
     # Standard deviation prior
-    prior(uniform(0, 10), class = "sigma", ub = 10),
+    brms::prior(uniform(0, 10), class = "sigma", ub = 10),
   )
 
+  return(priors)
 }
