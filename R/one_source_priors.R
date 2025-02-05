909189ba008b1938ff1e4ee0ee99b90064b18b54
Your Correct Name Here <Your Correct Email Here>
2025-02-05 13:13:09 -0500

    Updated text

--- R/one_source_priors.R
+++ R/one_source_priors.R
@@ -1,10 +1,11 @@
 #' Bayesian priors - One Source Trophic Position
 #'
-#' Create priors for one source trophic position model derived from [Post 2002]()
+#' Create priors for one source trophic position model derived from
+#' [Post 2002](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1890/0012-9658%282002%29083%5B0703%3AUSITET%5D2.0.CO%3B2).
 #'
-#' @param n1 mean prior value for \eqn{\delta}^15 N for baseline used in the model.
+#' @param n1 mean prior value for \eqn{\delta}\eqn{^1}\eqn{^5}N for baseline used in the model.
 #' Defaults to 9.
-#' @param n1_sigma variance of n1 prior value for \eqn{\delta}^15 N for baseline used in the model.
+#' @param n1_sigma variance for n1 priors that are \eqn{\delta}\eqn{^1}\eqn{^5}N for baseline used in the model.
 #' Defaults to 1.
 #'
 #' @import brms
