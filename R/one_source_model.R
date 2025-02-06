166c2e66f36d1ef9ab2b3d4a6af719a0c1fdee63
Your Correct Name Here <Your Correct Email Here>
2025-02-06 08:42:37 -0500

    Added info about one source model data requirements

--- R/one_source_model.R
+++ R/one_source_model.R
@@ -6,16 +6,24 @@
 #' \eqn{\delta}\eqn{^1}\eqn{^5}N = n1 + dn * (tp - \eqn{\lambda})
 #'
 #' \eqn{\delta}\eqn{^1}\eqn{^5}N are values from the consumer,
-#' n1 is \eqn{\delta}\eqn{^1}\eqn{^5}N  values of baseline 1,
+#' n1 is \eqn{\delta}\eqn{^1}\eqn{^5}N values of baseline 1,
 #' \eqn{\Delta}N is the trophic discrimination factor for N (i.e., mean of 3.4),
 #' tp is trophic position, and \eqn{\lambda} is the trophic level of baselines which are often a primary consumer (e.g., 2).
 #'
 #' Posterior distributions of of n1, dn, and tp will be estimated.
 #'
+#' @details
+#' The data supplied to `brms()` needs to have the following variables, `d15n`
+#' which is the eqn{\delta}\eqn{^1}\eqn{^5}N of the consumers, `n1` which is
+#' \eqn{\delta}\eqn{^1}\eqn{^5}N values of baseline 1, and
+#' `lambda` which is usually `2 `otherwise `brms()` will error.
+#'
 #' @import brms
+#' @seealso [brms::brms()]
 #' @export
 
 one_source_model <- function() {
+
   model <- brms::bf(
     # Likelihood function
     d15n ~ n1 + dn * (tp - lambda),
