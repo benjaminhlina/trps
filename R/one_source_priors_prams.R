8b4c86356148da1dbd604ff60e44e057fa96888f
Your Correct Name Here <Your Correct Email Here>
2025-02-06 07:55:29 -0500

    Add info on one_source_params

--- R/one_source_priors_prams.R
+++ R/one_source_priors_prams.R
@@ -3,6 +3,9 @@
 #' Adjust priors for one source trophic position model derived from
 #' [Post 2002](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1890/0012-9658%282002%29083%5B0703%3AUSITET%5D2.0.CO%3B2).
 #'
+#' This function allows the user to adjust the prior mean and variance for \eqn{\delta}\eqn{^1}\eqn{^5}N for baseline.
+#' To be used with `one_source_priors` and `one_source_model`.
+#'
 #' @param n1 mean prior value for \eqn{\delta}\eqn{^1}\eqn{^5}N for baseline used in the model.
 #' Defaults to 9.
 #' @param n1_sigma variance for n1 priors that are \eqn{\delta}\eqn{^1}\eqn{^5}N for baseline used in the model.
@@ -11,6 +14,7 @@
 #' You can adjust the prior mean and variance for \eqn{\delta}\eqn{^1}\eqn{^5}N by supplying `n1` and `n1_sigma` with a numerical value.
 #'
 #' @import brms
+#' @seealso [one_source_pirors()] and [one_source_model()]
 #' @export
 
 one_source_priors_prams <- function(
