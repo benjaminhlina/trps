d7d82b8dab2622a7f5c1dc22494bdc5a4efb5b81
Your Correct Name Here <Your Correct Email Here>
2025-02-06 07:18:58 -0500

    Added error tests for one_source_priors_prams

--- tests/testthat/test-one_source_priors_prams.R
+++ tests/testthat/test-one_source_priors_prams.R
@@ -14,16 +14,25 @@ test_that("test one-source priors mean", {
 }
 )
 
+#  tests for the one_source_priors_prams fails when given not. a numeric
 
-test_that("test one-source priors variance ", {
 
-  pri_pram <- one_source_priors_prams()
+test_that("test one-source priors fails n1", {
 
-  expected_n1_sigma <- 1
+  expect_error(one_source_priors_prams(n1 = "test"),
+               regexp = "`n1` argument must be a numerical value")
 
-  results_n1_sigma <- pri_pram$n1_sigma$sdata
 
-  expect_equal(results_n1_sigma, expected_n1_sigma)
+}
+)
+
+
+test_that("test priors fails n1_sigam ", {
+
+  expect_error(one_source_priors_prams(n1_sigma = "test"),
+               regexp = "`n1_sigma` argument must be a numerical value"
+               )
+
 
 
 }
