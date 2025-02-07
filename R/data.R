b40284c10d2266e516a0c3da38232dadcecc7463
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-07 13:14:11 -0500

    Added example data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' A `data.frame` of stable isotope data for lake trout used in one source model
+#'
+#' Stable isotope data (SIA) for \eqn{\delta}\eqn{^1}\eqn{^3}C and
+#' \eqn{\delta}\eqn{^1}\eqn{^5}N for lake trout collected from an ecoregion in
+#' Lake Ontario.
+#'
+#' @format `data.frame` containing 87 rows and 5 variables
+#'  \describe{
+#'    \item{common_name}{name of the spcies (i.e., lake trout)}
+#'    \item{ecoregion}{ecoregion where samples were collected}
+#'    \item{d13c}{observed values for \eqn{\delta}\eqn{^1}\eqn{^3}C}
+#'    \item{d15n}{observed values for \eqn{\delta}\eqn{^1}\eqn{^5}N}
+#'    \item{lambda}{trophic position of baseline used in this data (i.e., 2)}
+#' }
+#'
+"example_data_os"
