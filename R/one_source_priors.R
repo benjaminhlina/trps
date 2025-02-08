b93e7ba10bf8705cb35d78f4f6642644819a768b
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-08 18:45:36 -0500

    Added argument bp (baseline priors) as logical to change prior structure if baseline priors are desired or if raw data is to be used

--- R/one_source_priors.R
+++ R/one_source_priors.R
@@ -3,22 +3,60 @@
 #' Create priors for one source trophic position model derived from
 #' [Post 2002](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1890/0012-9658%282002%29083%5B0703%3AUSITET%5D2.0.CO%3B2).
 #'
+#' @param bp logical value that controls whether  priors are
+#' supplied to the model for \eqn{\delta}\eqn{^1}\eqn{^5}N baseline. Default is
+#' `FALSE` meaning the model will not be supplied priors, however, the supplied
+#' `data.frame` needs values for \eqn{\delta}\eqn{^1}\eqn{^5}N baseline.
+#'
 #' @import brms
 #' @export
 
-one_source_priors <- function() {
-  #  set priors
-  priors <- c(
-    # Baseline δ15N (n1)
-    brms::prior(normal(n1, n1_sigma), class = "b", coef = "Intercept", nlpar = "n1"),
-    # Trophic enrichment factor (ΔN)
-    brms::prior(normal(dn, dn_sigma), class = "b", coef = "Intercept", nlpar = "dn"),
-    # Trophic Position (tp)
-    brms::prior(uniform(tp_lb, tp_ub), class = "b", nlpar = "tp",
-          lb = tp_lb, ub = tp_ub),
-    # Standard deviation prior
-    brms::prior(uniform(sigma_lb, sigma_ub), class = "sigma", ub = sigma_ub)
-  )
+one_source_priors <- function(bp = FALSE) {
+
+  if (!(is.logical(bp))) {
+
+    cli::cli_abort(c(
+      "`bp` argument must be a logical value",
+      "i" = "Please provide TRUE or FALSE"
+    ))
+  }
+
+
+  if (isFALSE(bp)) {
+
+    #  set priors
+    priors <- c(
+      # Trophic enrichment factor (ΔN)
+      brms::prior(normal(dn, dn_sigma),
+                  class = "b",
+                  coef = "Intercept",
+                  nlpar = "dn"),
+      # Trophic Position (tp)
+      brms::prior(uniform(tp_lb, tp_ub), class = "b", nlpar = "tp",
+                  lb = tp_lb, ub = tp_ub),
+      # Standard deviation prior
+      brms::prior(uniform(sigma_lb, sigma_ub), class = "sigma", ub = sigma_ub)
+    )
+  }
+
+  if (isTRUE(bp)) {
+    #  set priors
+    priors <- c(
+      # Baseline δ15N (n1)
+      brms::prior(normal(n1, n1_sigma), class = "b", coef = "Intercept", nlpar = "n1"),
+      # Trophic enrichment factor (ΔN)
+      brms::prior(normal(dn, dn_sigma), class = "b",
+                  # coef = "Intercept",
+                  nlpar = "dn", lb = 3, ub = 4),
+      # Trophic Position (tp)
+      brms::prior(uniform(tp_lb, tp_ub), class = "b", nlpar = "tp",
+                  lb = tp_lb, ub = tp_ub),
+      # Standard deviation prior
+      brms::prior(uniform(sigma_lb, sigma_ub), class = "sigma", ub = sigma_ub)
+    )
+  }
+
+
 
   return(priors)
 }
