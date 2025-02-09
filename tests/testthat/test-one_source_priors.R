9e71ced1fe56db521126d6fcf07caecb47e946de
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-09 16:29:19 -0500

    Added test to check if bp argument does what it needs to do and errors appropratiely

--- tests/testthat/test-one_source_priors.R
+++ tests/testthat/test-one_source_priors.R
@@ -9,12 +9,38 @@ test_that("test one-source priors", {
                 "normal(dn, dn_sigma)",
                 "uniform(tp_lb, tp_ub)",
                 "uniform(sigma_lb, sigma_ub)"
-                )
+  )
 
   results <- pri$prior
 
 
   expect_equal(results, expected)
+}
+
+)
+# tests for the one_source_priors
+test_that("test one-source priors without baseline", {
+
+  pri <- one_source_priors()
+
+  expected <- c("normal(dn, dn_sigma)",
+                "uniform(tp_lb, tp_ub)",
+                "uniform(sigma_lb, sigma_ub)"
+  )
+
+  results <- pri$prior
+
+
+  expect_equal(results, expected)
+}
+
+)
+test_that("test one-source priors fail if given not logical", {
+
+  expect_error(
+    one_source_priors(bp = TRU),
+    regexp = "`bp` argument must be a logical value"
+  )
 }
 
 )
