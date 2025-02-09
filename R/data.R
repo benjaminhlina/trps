e66f61c753932d14da2e8383c0bac8561ee5593e
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-09 16:13:21 -0500

    Added id column to as a row id to the dataframe.

--- R/data.R
+++ R/data.R
@@ -4,8 +4,9 @@
 #' \eqn{\delta}\eqn{^1}\eqn{^5}N for lake trout collected from an ecoregion in
 #' Lake Ontario.
 #'
-#' @format `data.frame` containing 87 rows and 5 variables
+#' @format `data.frame` containing 87 rows and 6 variables
 #'  \describe{
+#'    \item{id}{row id number}
 #'    \item{common_name}{name of the spcies (i.e., Lake Trout)}
 #'    \item{ecoregion}{ecoregion where samples were collected}
 #'    \item{d13c}{observed values for \eqn{\delta}\eqn{^1}\eqn{^3}C}
