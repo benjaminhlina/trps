c67ad83806e15ec30f02164980087471b3bed2c7
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-11 10:15:18 -0500

    Updated manual for add_alpha

--- R/add_alpha.R
+++ R/add_alpha.R
@@ -1,14 +1,27 @@
-#' Calculate and add alpha
+#' Calculate and add \eqn{\alpha}
 #'
-#' Calculate alpha for a two source trophic position model derived from
-#' [Post 2002](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1890/0012-9658%282002%29083%5B0703%3AUSITET%5D2.0.CO%3B2).
+#' Calculate \eqn{\alpha} for a two source trophic position model using
+#' equations from [Post 2002](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1890/0012-9658%282002%29083%5B0703%3AUSITET%5D2.0.CO%3B2).
+#'
+#' \eqn{\alpha} = (\eqn{\delta}\eqn{^1}\eqn{^3}C[c] - \eqn{\delta}\eqn{^1}\eqn{^3}C[p]) /
+#' (\eqn{\delta}\eqn{^1}\eqn{^3}C[b] - \eqn{\delta}\eqn{^1}\eqn{^3}C[p])
+#'
+#' where \eqn{\delta}\eqn{^1}\eqn{^3}C[c] is the isotopic value for consumer,
+#' \eqn{\delta}\eqn{^1}\eqn{^3}C[p] is the mean isotopic value for pelagic
+#' baseline, and \eqn{\delta}\eqn{^1}\eqn{^3}C[b] is the mean isotopic value
+#' for benthic baseline.
 #'
 #' @param data data.frame of stable isotope samples with mean values for two
-#'             baselines. For aquatic ecosystems, baseline two needs to come
-#'             from a pelagic source, while baseline one needs to come from a
-#'             benthic source. Baseline \eqn{\delta}\eqn{^1}\eqn{^3}C columns
-#'             need to be named `c1` and `c2`, with the consumer's column named
-#'             `d13c`.
+#' baselines. For aquatic ecosystems, baseline two needs to come
+#' from a pelagic source, while baseline one needs to come from a
+#' benthic source. Baseline \eqn{\delta}\eqn{^1}\eqn{^3}C columns
+#' need to be named `c1` and `c2`, with the consumer's column named `d13c`.
+#'
+#' @returns a `data.frame` that has `alpha`, `min_alpha`, and `max_alpha` added.
+#'
+#' @examples
+#' example_iso_tsn |>
+#'   add_alpha()
 #' @import dplyr
 #' @export
 
@@ -42,8 +55,8 @@ add_alpha <- function(
   dat <- data |>
     dplyr::mutate(
       alpha = (d13c - c2) / (c1 - c2),
-      min_alpha = min(alpha),
-      max_alpha = max(alpha)
+      min_alpha = min(alpha, na.rm = TRUE),
+      max_alpha = max(alpha, na.rm = TRUE)
     )
 
 
