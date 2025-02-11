4d51817675657c5258999d54fc0a62e3440b701a
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-11 10:40:47 -0500

    Fixex equations

--- R/add_alpha.R
+++ R/add_alpha.R
@@ -3,18 +3,18 @@
 #' Calculate \eqn{\alpha} for a two source trophic position model using
 #' equations from [Post 2002](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1890/0012-9658%282002%29083%5B0703%3AUSITET%5D2.0.CO%3B2).
 #'
-#' \eqn{\alpha} = (\eqn{\delta}\eqn{^1}\eqn{^3}C[c] - \eqn{\delta}\eqn{^1}\eqn{^3}C[p]) /
-#' (\eqn{\delta}\eqn{^1}\eqn{^3}C[b] - \eqn{\delta}\eqn{^1}\eqn{^3}C[p])
+#' \deqn{\alpha = (\delta^1^3 Cc - \delta ^1^3Cp) /
+#' (\delta ^1^3Cb - \delta ^1^3Cb)}
 #'
-#' where \eqn{\delta}\eqn{^1}\eqn{^3}C[c] is the isotopic value for consumer,
-#' \eqn{\delta}\eqn{^1}\eqn{^3}C[p] is the mean isotopic value for pelagic
-#' baseline, and \eqn{\delta}\eqn{^1}\eqn{^3}C[b] is the mean isotopic value
+#' where \eqn{\delta^1^3 Cc} is the isotopic value for consumer,
+#' \eqn{\delta^1^3 Cp} is the mean isotopic value for pelagic
+#' baseline, and \eqn{\delta^1^3 Cb} is the mean isotopic value
 #' for benthic baseline.
 #'
 #' @param data data.frame of stable isotope samples with mean values for two
 #' baselines. For aquatic ecosystems, baseline two needs to come
 #' from a pelagic source, while baseline one needs to come from a
-#' benthic source. Baseline \eqn{\delta}\eqn{^1}\eqn{^3}C columns
+#' benthic source. Baseline \eqn{\delta^1^3 Cc} columns
 #' need to be named `c1` and `c2`, with the consumer's column named `d13c`.
 #'
 #' @returns a `data.frame` that has `alpha`, `min_alpha`, and `max_alpha` added.
