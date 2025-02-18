46a55b3f64885d10eb37c363d5d20aaf4f532092
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-17 20:47:12 -0500

    Initial commit of two_source_priors test

---
+++ tests/testthat/test-two_source_priors.R
@@ -0,0 +1,47 @@
+
+
+# tests for the one_source_priors
+test_that("test two-source priors", {
+
+  pri <- two_source_priors(bp = TRUE)
+
+  expected <- c("normal(n1, n1_sigma)",
+                "normal(n2, n2_sigma)",
+                "normal(dn, dn_sigma)",
+                "uniform(tp_lb, tp_ub)",
+                "uniform(sigma_lb, sigma_ub)"
+  )
+
+  results <- pri$prior
+
+
+  expect_equal(results, expected)
+}
+
+)
+# tests for the one_source_priors
+test_that("test two-source priors without baseline", {
+
+  pri <- two_source_priors()
+
+  expected <- c("normal(dn, dn_sigma)",
+                "uniform(tp_lb, tp_ub)",
+                "uniform(sigma_lb, sigma_ub)"
+  )
+
+  results <- pri$prior
+
+
+  expect_equal(results, expected)
+}
+
+)
+test_that("test two-source priors fail if given not logical", {
+
+  expect_error(
+    two_source_priors(bp = "TRU"),
+    regexp = "`bp` argument must be a logical value"
+  )
+}
+
+)
