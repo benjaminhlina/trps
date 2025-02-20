c2ed979c05d482501675d5f6bb364f32d3fe9085
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-20 10:32:02 -0500

    updated one-source manual

--- R/one_source_model.R
+++ R/one_source_model.R
@@ -3,26 +3,25 @@
 #' Bayesian model for trophic position using a one source model derived from
 #' [Post 2002](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1890/0012-9658%282002%29083%5B0703%3AUSITET%5D2.0.CO%3B2).
 #'
-#' \eqn{\delta}\eqn{^1}\eqn{^5}N = n1 + dn * (tp - \eqn{\lambda})
+#
+#' @param bp logical value that controls whether  priors are
+#' supplied to the model for \eqn{\delta^{15}}N baseline. Default is
+#' `FALSE` meaning the model will not be supplied priors, however, the supplied
+#' `data.frame` needs values for \eqn{\delta^{15}}N baseline (`n1`).
 #'
-#' \eqn{\delta}\eqn{^1}\eqn{^5}N are values from the consumer,
-#' n1 is \eqn{\delta}\eqn{^1}\eqn{^5}N values of baseline 1,
+#'
+#' @details
+#'
+#'  \deqn{\delta^{15}N = n1 + dn * (tp - \eqn{\lambda})}
+#'
+#' \eqn{\delta^{15}}N are values from the consumer,
+#' n1 is \eqn{\delta^{15}}N values of baseline 1,
 #' \eqn{\Delta}N is the trophic discrimination factor for N (i.e., mean of 3.4),
 #' tp is trophic position, and \eqn{\lambda} is the trophic level of baselines
 #' which are often a primary consumer (e.g., 2).
 #'
-#' @param bp logical value that controls whether  priors are
-#' supplied to the model for \eqn{\delta}\eqn{^1}\eqn{^5}N baseline. Default is
-#' `FALSE` meaning the model will not be supplied priors, however, the supplied
-#' `data.frame` needs values for \eqn{\delta}\eqn{^1}\eqn{^5}N baseline (`n1`).
-#'
-#'
-#' @details
-#' The data supplied to `brms()` needs to have the following variables, `d15n`
-#' which is the \eqn{\delta}\eqn{^1}\eqn{^5}N of the consumers, `n1` which is
-#' \eqn{\delta}\eqn{^1}\eqn{^5}N values of baseline 1 (only needed if priors
-#' aren't being supplied), and `lambda` which is usually `2 `
-#' otherwise `brms()` will error.
+#' The data supplied to `brms()` needs to have the following variables `d15n`,
+#'  `n1`, and `lambda` which is usually `2`.
 #'
 #' @return returns model structure for one source model to be used in a
 #' `brms()` call.
