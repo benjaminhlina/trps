1ffa8387cfba923898432596cb929814848177f5
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-24 23:33:43 -0500

    Removed ID column from all data objects except combined_iso will demonstrate this in vignette. Also changed baseline 1 to benthic source and baseline 2 to pelagic source.

--- R/data.R
+++ R/data.R
@@ -1,12 +1,11 @@
 #' Stable isotope data for lake trout (consumer)
 #'
-#' Stable isotope data (SIA) for \eqn{\delta^{13}}C and
-#' \eqn{\delta^ {15}}N for lake trout collected from an ecoregion in
+#' Stable isotope data (\eqn{\delta^{13}}C and
+#' \eqn{\delta^ {15}}N) for lake trout collected from an ecoregion in
 #' Lake Ontario.
 #'
 #' @format `data.frame` containing 30 rows and 6 variables
 #'  \describe{
-#'    \item{id}{row id number}
 #'    \item{common_name}{name of the spcies (i.e., Lake Trout)}
 #'    \item{ecoregion}{ecoregion where samples were collected}
 #'    \item{d13c}{observed values for \eqn{\delta^{13}}C}
@@ -15,45 +14,42 @@
 #'
 "consumer_iso"
 
-#' Stable isotope data for dreissenids (baseline 1)
+#' Stable isotope data for amphipods (baseline 1)
 #'
-#' Stable isotope data (SIA) for \eqn{\delta^{13}}C and
-#' \eqn{\delta^{15}}N dreissenid samples from an ecoregion in
-#' Lake Ontario.
-#'
-#' @format `data.frame` containing 12 rows and 5 variables
-#'  \describe{
-#'    \item{id}{row id number}
-#'    \item{common_name}{name of the spcies (i.e., Dreissenids)}
-#'    \item{ecoregion}{ecoregion where samples were collected}
-#'    \item{c1}{observed values for \eqn{\delta^{13}}C}
-#'    \item{n1}{observed values for \eqn{\delta^{15}}N}
-#' }
-#'
-"baseline_1_iso"
-
-#' Stable isotope data for amphipods (baseline 2)
-#'
-#' Stable isotope data (SIA) for \eqn{\delta^{13}}C and
-#' \eqn{\delta^{15}}N amphipods samples from an ecoregion in
+#' Stable isotope data (\eqn{\delta^{13}}C and
+#' \eqn{\delta^{15}}N) amphipods samples from an ecoregion in
 #' Lake Ontario.
 #'
 #' @format `data.frame` containing 14 rows and 5 variables
 #'  \describe{
-#'    \item{id}{row id number}
 #'    \item{common_name}{name of the spcies (i.e., Amphipoda)}
 #'    \item{ecoregion}{ecoregion where samples were collected}
 #'    \item{c2}{observed values for \eqn{\delta^{13}}C}
 #'    \item{n2}{observed values for \eqn{\delta^{15}}N}
 #' }
 #'
-"baseline_2_iso"
+"baseline_1_iso"
 
+#' Stable isotope data for dreissenids (baseline 2)
+#'
+#' Stable isotope data (\eqn{\delta^{13}}C and
+#' \eqn{\delta^{15}}N) dreissenid samples from an ecoregion in
+#' Lake Ontario.
+#'
+#' @format `data.frame` containing 12 rows and 5 variables
+#'  \describe{
+#'    \item{common_name}{name of the spcies (i.e., Dreissenids)}
+#'    \item{ecoregion}{ecoregion where samples were collected}
+#'    \item{c1}{observed values for \eqn{\delta^{13}}C}
+#'    \item{n1}{observed values for \eqn{\delta^{15}}N}
+#' }
+#'
+"baseline_2_iso"
 #' Stable isotope data for lake trout, dreissenids (pelagic baseline), and
 #' amphipods (benthic baseline)
 #'
-#' Stable isotope data (SIA) for \eqn{\delta^{13}}C and
-#' \eqn{\delta ^{15}}N for lake trout collected from an ecoregion in
+#' Stable isotope data (\eqn{\delta^{13}}C and
+#' \eqn{\delta ^{15}}N) for lake trout collected from an ecoregion in
 #' Lake Ontario. Values of \eqn{\delta ^{13}} C and
 #' \eqn{\delta ^{1}}N for a pelagic baseline
 #' (dreissenids; `c1` and `n1`) and a benthic baseline (amphipods;
@@ -63,12 +59,12 @@
 #' @format `data.frame` containing 30 rows and 13 variables
 #'  \describe{
 #'    \item{id}{row id number}
-#'    \item{common_name}{name of the spcies (i.e., Amphipoda)}
+#'    \item{common_name}{name of the spcies (i.e., Lake Trout)}
 #'    \item{ecoregion}{ecoregion where samples were collected}
 #'    \item{d13c}{observed values for \eqn{\delta^{13}}C of consumer}
 #'    \item{d15n}{observed values for \eqn{\delta^{15}}N of consumer}
 #'    \item{c1}{observed values for \eqn{\delta^{13}}C of baseline 1}
-#'    \item{n1}{observed values for \eqn{\delta^{15}}N of baseline 2}
+#'    \item{n1}{observed values for \eqn{\delta^{15}}N of baseline 1}
 #'    \item{c2}{observed values for \eqn{\delta^{13}}C of baseline 2}
 #'    \item{n2}{observed values for \eqn{\delta^{15}}N of baseline 2}
 #'    \item{c1_mean}{mean values for \eqn{\delta^{13}}C of baseline 1}
