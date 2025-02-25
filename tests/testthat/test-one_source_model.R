64c4bd89cb34f7eaff5c340f26bd78245c4d06cb
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-24 23:38:33 -0500

    Changed test to reflect chagne in one_source_model using l and not lambda

--- tests/testthat/test-one_source_model.R
+++ tests/testthat/test-one_source_model.R
@@ -3,7 +3,7 @@ test_that("test one-source model without baseline prior", {
   m <- one_source_model()
   # m$formula
   expected_wb <- formula(
-    d15n ~ n1 + dn * (tp - lambda)
+    d15n ~ n1 + dn * (tp - l)
   )
 
   # class(m$formula)
@@ -35,7 +35,7 @@ test_that("test one-source model with baseline prior", {
 
   m <- one_source_model(bp = TRUE)
 
-  el <- formula(d15n ~ n1 + dn * (tp - lambda))
+  el <- formula(d15n ~ n1 + dn * (tp - l))
 
 
 
