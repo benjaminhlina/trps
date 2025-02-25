c9bbecaf5ca6caad5b95e5e6b4816bc68177c1b2
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-24 23:39:03 -0500

    Changed two_source_model_acc to two_source_model_ac R file

--- R/two_source_model_ac.R
+++ R/two_source_model_ac.R
@@ -1,26 +1,26 @@
-#' Bayesian model - Two Source Trophic Position with \eqn{\alpha_c} and carbon source
+#' Bayesian model - Two Source Trophic Position with \eqn{\alpha_c}
 #'
-#' Bayesian model for trophic position using a two source model with \eqn{\alpha_c} and carbon source derived from
+#' Bayesian model for trophic position using a two source model with \eqn{\alpha_c} derived from
 #' [Post 2002](https://esajournals.onlinelibrary.wiley.com/doi/full/10.1890/0012-9658%282002%29083%5B0703%3AUSITET%5D2.0.CO%3B2) and [Heuvel et al. 2024](https://cdnsciencepub.com/doi/10.1139/cjfas-2024-0028)
 #'
 #' @param bp logical value that controls whether  priors are
-#' supplied to the model for \eqn{\delta}\eqn{^1}\eqn{^5}N pelagic and benthic
+#' supplied to the model for \eqn{\delta^{15}}N pelagic and benthic
 #' baselines. Default is `FALSE` meaning the model will not be supplied priors,
 #' however, the supplied `data.frame` needs values for
-#' \eqn{\delta}\eqn{^1}\eqn{^5}N each baseline (`n1` and `n2`).
+#' \eqn{\delta^{15}}N each baseline (`n1` and `n2`).
 #' @param lambda numerical value, `1` or `2`, that controls whether one or
 #' two lambdas are used. See details for equations and when to use `1` or `2`.
 #' Defaults to `1`.
 #' @return returns model structure for two source model to be used in a
 #' `brms()` call.
 #' @examples
-#' two_source_model_acc()
+#' two_source_model_ac()
 #'
 #' @seealso [brms::brms()]
 #' @import brms
 #' @export
 
-two_source_model_acc <- function(
+two_source_model_ac <- function(
     bp = FALSE,
     lambda = NULL) {
   if (!(is.logical(bp))) {
