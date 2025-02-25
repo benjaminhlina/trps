d9a0c58043db2634b578fda3d8819d3a4ae9ca5e
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-25 17:01:31 -0500

    Added alpha, alpha correct, and carbon mixing model

--- R/two_source_model_ac.R
+++ R/two_source_model_ac.R
@@ -11,6 +11,56 @@
 #' @param lambda numerical value, `1` or `2`, that controls whether one or
 #' two lambdas are used. See details for equations and when to use `1` or `2`.
 #' Defaults to `1`.
+#'
+#' @details
+#'
+#' \enumerate{
+#'   \item \deqn{\alpha = (\delta^{13} C_c - \delta ^{13}C_2) /
+#'   (\delta ^{13}C_1 - \delta ^{13}C_1)}
+#'
+#'   \item \deqn{\alpha = \alpha_c * (\alpha_{max} - \alpha_{min}) + \alpha_{min}}
+#'
+#'   \item \deqn{\delta^{13}C = c_1 \times \alpha_c + c_2 \times (1 - \alpha_c)}
+#'
+#'   \item \deqn{\delta^{15}N = \Delta n \times (tp - \lambda) + n_1 \times \alpha_c + n_2 \times (1 - \alpha_c)}
+#'
+#'                                                  Or
+#'
+#'   \item \deqn{\delta^{15}N = \Delta n \times (tp - (\lambda_1 \times \alpha_c + \lambda_2 \times (1 - \alpha_c))) + n_1 \times \alpha_c + n_2 \times (1 - \alpha_c)}
+#'    }
+#'
+#' For equation 1) \eqn{\delta^{13}C_c} is the isotopic value for consumer,
+#' \eqn{\delta^{13}C_2} is the mean isotopic value for baseline 2,
+#' and \eqn{\delta^{13} C_1} is the mean isotopic value for baseline 1.
+#'
+#' For equation 2) \eqn{\alpha} is being corrected using equations in
+#' [Heuvel et al. 2024](https://cdnsciencepub.com/doi/10.1139/cjfas-2024-0028).
+#' with \eqn{\alpha_c} being the corrected value (bound by 0 and 1),
+#' \eqn{\alpha_{min}} is the minimum \eqn{\alpha} value calculated
+#' using `add_alpha()` and \eqn{\alpha_{max}} being the maximum \eqn{\alpha}
+#' value calcualted using `add_alpha()`.
+#'
+#' For equation 3) is carbon source mixing model with \eqn{\delta^{13}}C being
+#' estimated using `c_1`, `c_2`  and \eqn{\alpha_c} calculated in equation 1.
+#'
+#' For equation 4) and 5) \eqn{\delta^{15}}N are values from the consumer,
+#' \eqn{n_1} is \eqn{\delta^{15}}N values of baseline 1, \eqn{n_2} is
+#' \eqn{\delta^{15}}N values of baseline 2,
+#' \eqn{\Delta}N is the trophic discrimination factor for N (i.e., mean of `3.4`),
+#' tp is trophic position, and \eqn{\lambda} or \eqn{\lambda_1} and
+#' \eqn{\lambda_2} are the trophic levels of
+#' baselines which are often a primary consumer (e.g., `2` or `2.5`).
+#'
+#' For equation 4), the data supplied to `brms()` when using baselines at the
+#' same trophic level (`lambda` argument set to `1`) needs to have the
+#' following variables, `d15n`, `n1`, `n2`, `l` (\eqn{\lambda}) which is
+#' usually `2`.
+#'
+#' For equation 5), ff using baselines at different trophic levels
+#' (`lambda` argument set to `2`) the dataframe needs to have `l1` and `l2`
+#' with a numerical value for each trophic level (e.g., `2` and `2.5`).
+#'
+#'
 #' @return returns model structure for two source model to be used in a
 #' `brms()` call.
 #' @examples
