e2e8633db091042b20363275f4dd4f98b666d826
benjaminhlina <benjamin.hlina@gmail.com>
2025-02-28 08:26:03 -0500

    Updated manual for combined_iso

--- R/data.R
+++ R/data.R
@@ -50,14 +50,14 @@
 #' and dreissenids (pelagic baseline; baseline 2),
 #'
 #' Stable isotope data (\eqn{\delta^{13}}C and
-#' \eqn{\delta ^{15}}N) for lake trout collected from an ecoregion in
+#' \eqn{\delta ^{15}}N) for lake trout collected from two ecoregions in
 #' Lake Ontario. Values of \eqn{\delta ^{13}}C and
 #' \eqn{\delta ^{15}}N for a benthic baseline (amphipods; baseline 1;
 #' `c1` and `n1`) and  pelagic baseline
 #' (dreissenids; baseline 2; `c2` and `n2`) with the means for each baseline
 #' calculated (`c1_mean`,  `n1_mean`, `c2_mean`, and `n2_mean`).
 #'
-#' @format `data.frame` containing 30 rows and 13 variables
+#' @format `data.frame` containing 117 rows and 13 variables
 #'  \describe{
 #'    \item{id}{row id number}
 #'    \item{common_name}{name of the spcies (i.e., Lake Trout)}
