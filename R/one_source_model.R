c8d4b216b861d5d24c9781c380e0e19cb61101be
benjaminhlina <benjamin.hlina@gmail.com>
2025-03-03 15:05:53 -0500

    Added that this needs to be the mean

--- R/one_source_model.R
+++ R/one_source_model.R
@@ -15,7 +15,7 @@
 #' \deqn{\delta^{15}N = \delta^{15} N_1  + \Delta N \times (tp - \lambda_1)}
 #'
 #' \eqn{\delta^{15}}N are values from the consumer,
-#' \eqn{\delta^{15} N_1} is \eqn{\delta^{15}}N values of baseline 1,
+#' \eqn{\delta^{15} N_1} is mean \eqn{\delta^{15}}N values of baseline 1,
 #' \eqn{\Delta}N is the trophic discrimination factor for N (i.e., `dn` mean
 #' of `3.4`), \eqn{tp} is trophic position, and \eqn{\lambda_1} is the
 #' trophic level of baselines which are often a primary consumer (e.g., `2`).
