ae0f4454bc3d3fc8a9689dba8333944fc55f7675
benjaminhlina <benjamin.hlina@gmail.com>
2025-03-03 11:36:39 -0500

    Chagned test to ar instead of ac

--- tests/testthat/test-two_source_model_ar.R
+++ tests/testthat/test-two_source_model_ar.R
@@ -1,9 +1,9 @@
 test_that("test two-source model without baseline prior", {
 
-  m <- two_source_model_ac()
+  m <- two_source_model_ar()
   # m$formula
   expected_wb <- formula(
-    d15n ~ dn * (tp - l1) + n1 * ac + n2 * (1 - ac)
+    d15n ~ dn * (tp - l1) + n1 * ar + n2 * (1 - ar)
   )
 
   # class(m$formula)
@@ -16,10 +16,10 @@ test_that("test two-source model without baseline prior", {
 )
 test_that("test two-source model without baseline prior", {
 
-  m <- two_source_model_ac()
+  m <- two_source_model_ar()
   # m$formula
   expected_wb <- formula(
-    d13c ~ (c1 * ac)  + (c2 * (1 - ac))
+    d13c ~ (c1 * ar)  + (c2 * (1 - ar))
   )
 
   # class(m$formula)
@@ -34,10 +34,10 @@ test_that("test two-source model without baseline prior", {
 
 test_that("test two-source model without baseline prior", {
 
-  m <- two_source_model_ac()
+  m <- two_source_model_ar()
   # m$formula
   expected_wb <- formula(
-    alpha ~ ac * (max_alpha - min_alpha) + min_alpha
+    alpha ~ ar * (max_alpha - min_alpha) + min_alpha
   )
 
   # class(m$formula)
@@ -50,11 +50,11 @@ test_that("test two-source model without baseline prior", {
 )
 test_that("test two-source model without baseline prior", {
 
-  m <- two_source_model_ac()
+  m <- two_source_model_ar()
 
 
   prform <- list(
-    ac = formula(ac ~ 1),
+    ar = formula(ar ~ 1),
      tp = formula(tp ~ 1),
      dn = formula(dn ~ 1)
 
@@ -69,11 +69,11 @@ test_that("test two-source model without baseline prior", {
 )
 test_that("test two-source model without baseline prior", {
 
-  m <- two_source_model_ac()
+  m <- two_source_model_ar()
 
 
   prform <- list(
-    ac = formula(ac ~ 1)
+    ar = formula(ar ~ 1)
 
 
   )
@@ -87,11 +87,11 @@ test_that("test two-source model without baseline prior", {
 )
 test_that("test two-source model without baseline prior", {
 
-  m <- two_source_model_ac()
+  m <- two_source_model_ar()
 
 
   prform <- list(
-    ac = formula(ac ~ 1)
+    ar = formula(ar ~ 1)
 
 
   )
@@ -110,10 +110,10 @@ test_that("test two-source model without baseline prior", {
 
 test_that("test two-source model with baseline prior", {
 
-  m <- two_source_model_ac(bp = TRUE)
+  m <- two_source_model_ar(bp = TRUE)
 
   el <- formula(
-    d15n ~ dn * (tp - l1) + n1 * ac + n2 * (1 - ac)
+    d15n ~ dn * (tp - l1) + n1 * ar + n2 * (1 - ar)
     )
 
 
@@ -129,11 +129,11 @@ test_that("test two-source model with baseline prior", {
 
 test_that("test two-source model with baseline prior", {
 
-  m <- two_source_model_ac(bp = TRUE)
+  m <- two_source_model_ar(bp = TRUE)
 
 
   prform <- list(
-    ac = formula(ac ~ 1),
+    ar = formula(ar ~ 1),
     n1 = formula(n1 ~ 1),
     n2 = formula(n2 ~ 1),
     tp = formula(tp ~ 1),
@@ -149,11 +149,11 @@ test_that("test two-source model with baseline prior", {
 )
 test_that("test two-source model with baseline prior", {
 
-  m <- two_source_model_ac(bp = TRUE)
+  m <- two_source_model_ar(bp = TRUE)
 
 
   prform <- list(
-    ac = formula(ac ~ 1),
+    ar = formula(ar ~ 1),
     c1 = formula(c1 ~ 1),
     c2 = formula(c2 ~ 1)
 
@@ -167,11 +167,11 @@ test_that("test two-source model with baseline prior", {
 )
 test_that("test two-source model with baseline prior", {
 
-  m <- two_source_model_ac(bp = TRUE)
+  m <- two_source_model_ar(bp = TRUE)
 
 
   prform <- list(
-    ac = formula(ac ~ 1)
+    ar = formula(ar ~ 1)
 
   )
 
@@ -186,7 +186,7 @@ test_that("test two-source model with baseline prior", {
 test_that("test two-source bp eorros", {
 
   expect_error(
-    two_source_model_ac(bp = "TRU")
+    two_source_model_ar(bp = "TRU")
   )
 
 
@@ -197,10 +197,10 @@ test_that("test two-source bp eorros", {
 
 test_that("test two-source model with multiple lambda baseline prior", {
 
-  m <- two_source_model_ac(lambda = 2)
+  m <- two_source_model_ar(lambda = 2)
   # m$formula
   expected_wb <- formula(
-    d15n ~ dn * (tp - (l1 * ac + l2 * (1 - ac))) + n1 * ac + n2 * (1 - ac)
+    d15n ~ dn * (tp - (l1 * ar + l2 * (1 - ar))) + n1 * ar + n2 * (1 - ar)
   )
 
   # class(m$formula)
@@ -215,11 +215,11 @@ test_that("test two-source model with multiple lambda baseline prior", {
 
 test_that("test two-source model and two lambdads without baseline prior", {
 
-  m <- two_source_model_ac(lambda = 2)
+  m <- two_source_model_ar(lambda = 2)
 
 
   prform <- list(
-    ac = formula(ac ~ 1),
+    ar = formula(ar ~ 1),
     tp = formula(tp ~ 1),
     dn = formula(dn ~ 1)
 
@@ -236,11 +236,11 @@ test_that("test two-source model and two lambdads without baseline prior", {
 
 test_that("test two-source model with baseline prior", {
 
-  m <- two_source_model_ac(bp = TRUE,
+  m <- two_source_model_ar(bp = TRUE,
                         lambda = 2)
 
   el <- formula(
-    d15n ~ dn * (tp - (l1 * ac + l2 * (1 - ac))) + n1 * ac + n2 * (1 - ac)
+    d15n ~ dn * (tp - (l1 * ar + l2 * (1 - ar))) + n1 * ar + n2 * (1 - ar)
   )
 
 
@@ -256,12 +256,12 @@ test_that("test two-source model with baseline prior", {
 
 test_that("test two-source model with baseline prior", {
 
-  m <- two_source_model_ac(bp = TRUE,
+  m <- two_source_model_ar(bp = TRUE,
                         lambda = 2)
 
 
   prform <- list(
-    ac = formula(ac ~ 1),
+    ar = formula(ar ~ 1),
     n1 = formula(n1 ~ 1),
     n2 = formula(n2 ~ 1),
     tp = formula(tp ~ 1),
@@ -278,7 +278,7 @@ test_that("test two-source model with baseline prior", {
 test_that("test two-source lamba errros", {
 
   expect_error(
-    two_source_model_ac(lambda = 3),
+    two_source_model_ar(lambda = 3),
     "`lambda` argument must be a numeric value and either `1` or `2`"
   )
 
