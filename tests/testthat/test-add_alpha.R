f54ec9223f242cf999eb8b159d4501422d68b434
benjaminhlina <benjamin.hlina@gmail.com>
2025-03-03 15:23:04 -0500

    Updated tests for erroring now that c1 and c2 are the correct names for add_alpha

--- tests/testthat/test-add_alpha.R
+++ tests/testthat/test-add_alpha.R
@@ -47,7 +47,7 @@ test_that("test check if alpha errors ", {
 
   expect_error(
     add_alpha(data = combined_iso[-c(4, 10, 12)]),
-    "The data.frame is missing: d13c, c1_mean, and c2_mean"
+    "The data.frame is missing: d13c, c1, and c2"
   )
 }
 )
