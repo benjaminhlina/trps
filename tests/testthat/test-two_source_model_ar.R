2a8f122235851c0392a8b6863ddb1101450219c9
benjaminhlina <benjamin.hlina@gmail.com>
2025-03-13 11:04:45 -0400

    Commetned out carbon model test as it needs to be moved to arc function testing

--- tests/testthat/test-two_source_model_ar.R
+++ tests/testthat/test-two_source_model_ar.R
@@ -14,22 +14,22 @@ test_that("test two-source model without baseline prior", {
 
 }
 )
-test_that("test two-source model without baseline prior", {
-
-  m <- two_source_model_ar()
-  # m$formula
-  expected_wb <- formula(
-    d13c ~ (c1 * ar)  + (c2 * (1 - ar))
-  )
-
-  # class(m$formula)
-  expect_equal(m$forms$d13c$formula,
-               expected_wb,
-               ignore_attr = TRUE)
-
-
-}
-)
+# test_that("test two-source model without baseline prior", {
+#
+#   m <- two_source_model_ar()
+#   # m$formula
+#   expected_wb <- formula(
+#     d13c ~ (c1 * ar)  + (c2 * (1 - ar))
+#   )
+#
+#   # class(m$formula)
+#   expect_equal(m$forms$d13c$formula,
+#                expected_wb,
+#                ignore_attr = TRUE)
+#
+#
+# }
+# )
 
 
 test_that("test two-source model without baseline prior", {
@@ -85,24 +85,24 @@ test_that("test two-source model without baseline prior", {
                ignore_attr = TRUE)
 }
 )
-test_that("test two-source model without baseline prior", {
-
-  m <- two_source_model_ar()
-
-
-  prform <- list(
-    ar = formula(ar ~ 1)
-
-
-  )
-
-
-
-  expect_equal(m$forms$d13c$pforms,
-               prform,
-               ignore_attr = TRUE)
-}
-)
+# test_that("test two-source model without baseline prior", {
+#
+#   m <- two_source_model_ar()
+#
+#
+#   prform <- list(
+#     ar = formula(ar ~ 1)
+#
+#
+#   )
+#
+#
+#
+#   expect_equal(m$forms$d13c$pforms,
+#                prform,
+#                ignore_attr = TRUE)
+# }
+# )
 
 
 
@@ -147,24 +147,24 @@ test_that("test two-source model with baseline prior", {
                ignore_attr = TRUE)
 }
 )
-test_that("test two-source model with baseline prior", {
-
-  m <- two_source_model_ar(bp = TRUE)
-
-
-  prform <- list(
-    ar = formula(ar ~ 1),
-    c1 = formula(c1 ~ 1),
-    c2 = formula(c2 ~ 1)
-
-  )
-
-
-  expect_equal(m$forms$d13c$pforms,
-               prform,
-               ignore_attr = TRUE)
-}
-)
+# test_that("test two-source model with baseline prior", {
+#
+#   m <- two_source_model_ar(bp = TRUE)
+#
+#
+#   prform <- list(
+#     ar = formula(ar ~ 1),
+#     c1 = formula(c1 ~ 1),
+#     c2 = formula(c2 ~ 1)
+#
+#   )
+#
+#
+#   expect_equal(m$forms$d13c$pforms,
+#                prform,
+#                ignore_attr = TRUE)
+# }
+# )
 test_that("test two-source model with baseline prior", {
 
   m <- two_source_model_ar(bp = TRUE)
