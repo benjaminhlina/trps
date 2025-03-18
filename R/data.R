22aa2535beeb7f8a39d645721186b2d0ba328632
benjaminhlina <benjamin.hlina@gmail.com>
2025-03-18 17:30:28 -0400

    Added manual for model outputs

--- R/data.R
+++ R/data.R
@@ -77,4 +77,31 @@
 #'
 "combined_iso"
 
+#' Model Output - One Source Model
+#' One source model output from `{brms}` model.
+#'
+#' @format `brms` object
+#'
+"model_output_os"
 
+#' Model Output - One Source Model - Multiple Groups
+#' One source model output from `{brms}` model for multiple groups.
+#'
+#' @format `brms` object
+#'
+"model_output_os_mg"
+
+#' Model Output - Two Source Model
+#' Two source model output from `{brms}` model. .
+#'
+#' @format `brms` object
+#'
+"model_output_ts"
+
+
+#' Model Output - Two Source Model - \eqn{\alpha_r}
+#' Two source model with \eqn{\alpha_r} output from `{brms}` model. .
+#'
+#' @format `brms` object
+#'
+"model_output_tsar"
